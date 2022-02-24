3dfc1173d49fd1e67fa58b6efa69970145623fea
RyosukeSomeya <rx.0.0618@gmail.com>
2022-02-25 01:52:47 +0900

    seeder修正

--- laravel_app/todo_list/database/seeders/DatabaseSeeder.php
+++ laravel_app/todo_list/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Todo::factory(3)->create();
+        $this->call([
+            TodoSeeder::class,
+        ]);
     }
 }
